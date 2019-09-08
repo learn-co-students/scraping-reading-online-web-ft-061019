70dbcd3550e3d7edf46fd053f3a43d6efcd831f7
github username <github email address>
2019-09-08 14:37:59 -0400

    little confusing

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
 
+doc = Nokogiri::HTML(open("http://flatironschool.com"))
+courses = doc.css(".tout__label.heading.heading--level-4")
+ 
+courses.each do |course|
+  puts course.text.strip
+end
+doc.css(".site-header__hero__headline").text
 
-
+p doc.css(".tout__label.heading.heading--level-4")[0].attributes
